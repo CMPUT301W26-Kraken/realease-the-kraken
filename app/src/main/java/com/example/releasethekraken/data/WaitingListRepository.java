565fb88287e06d0d087c03da5e0a6ca13f11737e
Alexk177 <Kalmykov@ualberta.ca>
2026-03-02 12:36:07 -0700

    created packages: models, logic, data. Under Models, I created Event.jave to add event with registration window fields

---
+++ app/src/main/java/com/example/releasethekraken/data/WaitingListRepository.java
@@ -0,0 +1,4 @@
+package com.example.releasethekraken.data;
+
+public class WaitingListRepository {
+}
---
+++ app/src/main/java/com/example/releasethekraken/models/WaitingListEntry.java
@@ -0,0 +1,4 @@
+package com.example.releasethekraken.models;
+
+public class WaitingListEntry {
+}
