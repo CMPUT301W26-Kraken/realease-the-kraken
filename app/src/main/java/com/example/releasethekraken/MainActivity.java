4f010e9c42c37be9cb0c3b5ee6f4ea90441bde2c
Nav <n.schar110@gmail.com>
2026-03-10 13:33:34 -0600

    Configure Firebase

--- app/src/main/java/com/example/releasethekraken/MainActivity.java
+++ app/src/main/java/com/example/releasethekraken/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.releasethekraken;
 
 import android.os.Bundle;
 
+import android.Manifest;
+import android.os.Build;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        // Retrieve the FCM registration token and log it to verify Firebase is connected
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                String token = task.getResult();
+                Log.d("FCM", "Token: " + token);
+            }
+        });
+
+        // Firebase connection test - temporary test remove if needed)
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        Log.d("StorageTest", "Storage connected: " + storageRef.getBucket());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
