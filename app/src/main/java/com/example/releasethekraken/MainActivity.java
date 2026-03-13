07b12e62dcfb2ee6e110e085ebf0c72f04f11327
Nav <n.schar110@gmail.com>
2026-03-13 16:16:53 -0600

    fixed bugs, removed partial implemenations for backend

--- app/src/main/java/com/example/releasethekraken/MainActivity.java
+++ app/src/main/java/com/example/releasethekraken/MainActivity.java
@@ -11,9 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.releasethekraken.controller.SessionManager;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,11 +39,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("FCM", "Token: " + token);
             }
         });
-
-        // Confirms Firebase Storage bucket is reachable — remove once Firebase Storage is successfully integrated
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-        Log.d("StorageTest", "Storage connected: " + storageRef.getBucket());
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
